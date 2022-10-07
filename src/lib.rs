c6879a0ed1042e4b65a3c120c1733d202e675bb4
MikeKondr99@gmail.com <MikeKondr99@gmail.com>
2022-10-07 14:49:25 +0300

    init

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+mod tests;
+//
+pub fn sqr(n: i32) -> i32 {
+    n * n
+}
---
+++ src/tests.rs
@@ -0,0 +1,9 @@
+use crate::sqr;
+#[cfg(test)]
+//
+#[test]
+fn it_works() {
+    assert!(sqr(2) == 4);
+    assert!(sqr(3) == 9);
+    assert!(sqr(4) == 16);
+}
